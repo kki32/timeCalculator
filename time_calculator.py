78308945acf5bf7da0646aeff254a7aac08e0785
Suchartee Kitisopakul (Alice) <suchartee.k@gmail.com>
2017-11-03 18:07:02 -0700

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -25,12 +25,12 @@
 #root.mainloop()
 
 
-def time_find(dated, datem, datey, time, country):
+def time_find(dated, datem, datey, time, country):  #day, month, time, country
     pass
 
 from geopy import geocoders # $ pip  install geopy
-from datetime import datetime,timedelta
-import pytz
+from datetime import datetime, timedelta # datetime = A combination of a date and a time. Attributes: year, month, day, hour, minute, second, microsecond, and tzinfo , timedelta = A duration expressing the difference between two date, time, or datetime instances to microsecond resolution
+import pytz # $ python -m pip install pytz
 import calendar
 
 #g = geocoders.GoogleV3 # set variable from Google
@@ -42,27 +42,36 @@ import calendar
 #weekday = now.weekday() 
 #print(calendar.day_name[weekday])
 
-def countryFind(country):
+#def pretty_print(now):
+    #print(now.month, now.day, now.year, now.hour, now.minute)
+
+
+
+# this function return the city's time at the moment
+def timeFind(city):
     g = geocoders.GoogleV3() # set variable from Google
-    place, (lat, lng) = g.geocode(country) # get country
-    timezone = g.timezone((lat, lng)) # return pytz timezone object = get that country's timezone  
-    
-    now = datetime.now(timezone) # you could pass `timezone` object here
+    place, (lat, lng) = g.geocode(city) # get (latitude, longitude) of city
+    timezone = g.timezone((lat, lng)) # return pytz timezone object == get the city's timezone  
+    now = datetime.now(timezone) # you could pass 'timezone' object here
     return now
     #weekday = now.weekday() 
     #print(calendar.day_name[weekday])    
 
-def pretty_print(now):
-    print(now.month, now.day, now.year, now.hour, now.minute)
-country = 'christchurch'
-origin = countryFind(country)
-country = ('los angeles')
-destination = countryFind(country.lower())
-print(origin)
-print(destination)
 
 
-fmt = '%Y-%m-%d %H:%M'
+# start here
+
+city = raw_input('Enter the origin city: ')   # get origin
+origin = timeFind(city.lower())
+city = raw_input('Enter the destination city: ')  #get destination
+destination = timeFind(city.lower())
+
+print("Origin city: " + str(origin))
+print("Destination city: " + str(destination))
+
+
+
+fmt = '%Y-%m-%d %H:%M'  # set format
 origin = origin.strftime(fmt)
 destination = destination.strftime(fmt)
 d1 = datetime.strptime(origin, fmt)
@@ -72,5 +81,7 @@ diff = d2 - d1
 
 diff_hours = ((diff.days * 24 * 60) + (diff.seconds/60))/60 # calculate the hours difference
 
-
-print('+' + str(diff_hours))
+if (str(diff_hours)[0] == '-'):
+    print(str(diff_hours) + ' hours from origin city')
+else:
+    print('+' + str(diff_hours) + ' hours from origin city')
